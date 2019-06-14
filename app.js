af7a8be0a4f30927066ba10a1250bd8f6a4e157a
archana.karki <archana.karki@edu.turkuamk.fi>
2019-06-15 02:54:42 +0300

    Layout updated

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-$('img').hover(function(){
-    $(this).attr('width', '400px');
+$('#basic_website').mouseover(function(){
+    $(this).attr('src', 'assets/snaps/basic_website.png');
+});
+$('#basic_website').mouseout(function(){
+    $(this).attr('src', 'assets/snaps/basic_website.png');
+});
+
+
+
+$('#todo').mouseover(function(){
+    $(this).attr('src', 'assets/snaps/color_todo.png');
+});
+$('#todo').mouseout(function(){
+    $(this).attr('src', 'assets/snaps/todo.png');
+});
+
+
+$('#pwa').mouseover(function(){
+    $(this).attr('src', 'assets/snaps/color_pwa.png');
+});
+$('#pwa').mouseout(function(){
+    $(this).attr('src', 'assets/snaps/pwa.png');
 });
